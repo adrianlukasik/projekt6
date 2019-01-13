5f51f991902e8704010e4a38540ba95f3e145fa6
Mateusz Sieniawski <Cheetar@users.noreply.github.com>
2019-01-13 22:08:53 +0100

    Added asserts on rebelships speed.

--- rebelfleet.cc
+++ rebelfleet.cc
@@ -1,17 +1,30 @@
 #include "rebelfleet.h"
 
+#define MIN_STARCRUISER_SPEED 99999
+#define MAX_STARCRUISER_SPEED 299795
+#define MIN_EXPLORER_SPEED 299796
+#define MAX_EXPLORER_SPEED 2997960
+#define MIN_XWING_SPEED 299796
+#define MAX_XWING_SPEED 2997960
+
 Speed RebelStarship::getSpeed() const {
     return speed;
 }
 
 Explorer::Explorer(ShieldPoints shld, Speed spd)
-    : shield(shld), speed(spd) {}
+    : shield(shld), speed(spd) {
+        assert(MIN_EXPLORER_SPEED <= spd && spd <= MAX_EXPLORER_SPEED)
+    }
 
 StarCruiser::StarCruiser(ShieldPoints shld, Speed spd, AttackPower atck)
-    : shield(shld), speed(spd), attack(atck) {}
+    : shield(shld), speed(spd), attack(atck) {
+        assert(MIN_STARCRUISER_SPEED <= spd && spd <= MAX_STARCRUISER_SPEED)
+    }
 
 XWing::XWing(ShieldPoints shld, Speed spd, AttackPower atck)
-    : shield(shld), speed(spd), attack(atck) {}
+    : shield(shld), speed(spd), attack(atck) {
+        assert(MIN_XWING_SPEED <= spd && spd <= MAX_XWING_SPEED)
+    }
 
 
 std::shared_ptr<Explorer> createExplorer(ShieldPoints shield, Speed speed) {
